7a9b5dc4c930448c65e3d6df0923c85d31754e7d
tralfamadorians <kellyincville@gmail.com>
2020-02-14 23:08:38 +0000

    organized and added new folder for ch_2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import pandas as pd, numpy, matplotlib
 print(matplotlib.__version__, pd.__version__, numpy.__version__)
 
-from small_exercises.fiveTimes import *
+from small_exercises.ch_2.fiveTimes import *
